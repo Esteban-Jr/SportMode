4c37ba8bb2328341f360b2ec1af339a397266a8f
Esteban Gonzalez <jotaere@MBP-de-Esteban.lan>
2026-04-16 00:43:34 +0100

    Add Heroku deployment configuration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sportsmode.wsgi:application
--- sportsmode/settings.py
+++ sportsmode/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -111,12 +112,16 @@ WSGI_APPLICATION = 'sportsmode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = config('DATABASE_URL', default='')
+if DATABASE_URL:
+    DATABASES = {'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
